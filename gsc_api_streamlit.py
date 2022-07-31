37304a78035f032dcdc588a5b5bc1f8d30537902
ilaygranot <99613724+ilaygranot@users.noreply.github.com>
2022-07-31 16:35:19 +0300

    Update gsc_api_streamlit.py

--- gsc_api_streamlit.py
+++ gsc_api_streamlit.py
@@ -23,7 +23,7 @@ else:
     layout = "wide" if st.session_state.widen else "centered"
 
 st.set_page_config(
-    layout=layout, page_title="Google Search Console Connector", page_icon="https://iconarchive.com/download/i79580/martz90/hex/files.ico"
+    layout=layout, page_title="Google Search Console API Explorer", page_icon="https://iconarchive.com/download/i79580/martz90/hex/files.ico"
 )
 
 # -----------------------------------------------------
@@ -355,22 +355,6 @@ with tab2:
     st.write(
         """
     #### About this app
-    * ✔️ One-click connect to the [Google Search Console API](https://developers.google.com/webmaster-tools)
-    * ✔️ Easily traverse your account hierarchy
-    * ✔️ Go beyond the [1K row UI limit](https://www.gsqi.com/marketing-blog/how-to-bulk-export-search-features-from-gsc/)
-    * ✔️ Enrich your data querying with multiple dimensions layers and extra filters!
-    ✍️ You can read the blog post [here](https://blog.streamlit.io/p/e89fd54e-e6cd-4e00-8a59-39e87536b260/) for more information.
-    #### Going beyond the `10K` row limit
-    * There's a `10K` row limit per API call on the [Cloud](https://streamlit.io/cloud) version to prevent crashes.
-    * You can remove that limit by forking this code and adjusting the `RowCap` variable in the `streamlit_app.py` file
-    #### Kudos
-    This app relies on Josh Carty's excellent [Search Console Python wrapper](https://github.com/joshcarty/google-searchconsole). Big kudos to him for creating it!
-    #### Questions, comments, or report a 🐛?
-    * If you have any questions or comments, please DM [me](https://twitter.com/DataChaz). Alternatively, you can ask the [Streamlit community](https://discuss.streamlit.io).
-    * If you find a bug, please raise an issue in [Github](https://github.com/CharlyWargnier/google-search-console-connector/pulls).
-    #### Known bugs
-    * You can filter any dimension in the table even if the dimension hasn't been pre-selected. I'm working on a fix for this.
-    
     """
     )
     
